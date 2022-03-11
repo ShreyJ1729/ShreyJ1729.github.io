3aa2bdafe2b4b399bf07768d12f61a476c5e0e95
Shrey Joshi <shreyjoshi2004@gmail.com>
2022-03-11 00:43:09 -0600

    made responsive

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const updateAge = () => {
-    const bday = new Date("Tue Aug 3 2004").getTime();
-    const ageMs = Date.now() - bday
-    const age = ageMs/1000/60/60/24/365.25
-    document.getElementById("age").innerHTML = "Age: " + age.toPrecision(10);
-    requestAnimationFrame(updateAge);
-}
+  const bday = new Date("Tue Aug 3 2004").getTime();
+  const ageMs = Date.now() - bday;
+  const age = ageMs / 1000 / 60 / 60 / 24 / 365.25;
+  document.getElementById("age").innerHTML = "Current Age: " + age.toPrecision(10);
+  requestAnimationFrame(updateAge);
+};
 
+const OpenLinksInNewTab = () => {
+  const links = document.getElementsByTagName("a");
+  
+  for (let i = 0; i < links.length; i++) {
+    last = links[i].href.split("/").pop();
+
+    if (!last.includes("#")) {
+      links[i].target = "_blank";
+    }
+  }
+};
+
+OpenLinksInNewTab();
 requestAnimationFrame(updateAge);
