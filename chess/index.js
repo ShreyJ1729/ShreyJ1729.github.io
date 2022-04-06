27f2e6d722496528fcd012d2cc3781790d76306f
Shrey Joshi <shreyjoshi2004@gmail.com>
2022-04-06 01:42:52 -0500

    updated chess

--- chess/index.js
+++ chess/index.js
@@ -1,42 +1,158 @@
 import { Chess } from "./chess.js";
 
-var board = Chessboard("board", {
-  draggable: true,
-});
-board.start();
+class ChessGame {
+  constructor(whitePlayer, blackPlayer) {
+    this.white = whitePlayer;
+    this.black = blackPlayer;
 
-const game = new Chess();
+    // determine gameMode
+    if (this.white == "h" && this.black == "h") this.gameMode = "hvh";
+    else if (this.white !== this.black) this.gameMode = "hvc";
+    else this.gameMode = "cvc";
 
-console.log(game.pgn());
+    console.log("Game Mode:", this.gameMode);
 
-$("#startBtn").on("click", () => {
-  if (confirm("This will reset the board. Are you sure?")) {
-    board.start();
-    game.reset();
-    console.log("reset game!");
+    // initialist game state and board UI
+    this.game = new Chess();
+    this.board = Chessboard("board", {
+      draggable: this.gameMode == "cvc" ? false : true, // lock board in cvc gameMode
+      showNotation: false,
+      showErrors: "console",
+      position: "start",
+      moveSpeed: "10",
+      orientation: this.getInitialOrientation(),
+      onDragStart: this.onDragStart.bind(this),
+      onDrop: this.onDrop.bind(this),
+    });
   }
-});
 
-$("#moveBtn").on("click", () => {
-//   if (!game.game_over()) {
-    move();
-//   }
-  console.log(game.fen());
-});
-
-$("#moveToEndBtn").on("click", () => {
-  while (!game.game_over()) {
-    move();
+  // gets initial board UI orientation from gameMode
+  getInitialOrientation() {
+    if (this.white == this.black) return "white";
+    if (this.white == "h") return "white";
+    return "black";
   }
-  console.log(game.pgn());
-});
 
-const move = () => {
-  let moves = game.moves({ verbose: true });
+  onDragStart(source, piece, position, orientation) {
+    // don't move the other side's pieces
+    if (
+      (orientation === "white" && piece.includes("b")) ||
+      (orientation === "black" && piece.includes("w"))
+    ) {
+      return false;
+    }
+  }
 
-  let move = moves[Math.floor(Math.random() * moves.length)];
-  game.move(move);
+  onDrop(source, target, piece, newPos, oldPos, orientation) {
+    // validate the move
+    let desiredMove = { from: source, to: target };
+    let validMoves = this.game.moves({ verbose: true });
+    let isMoveValid = false;
 
-  let boardMove = move.from + "-" + move.to;
-  board.move(boardMove);
+    validMoves.forEach((move) => {
+      if (desiredMove.from == move.from && desiredMove.to == move.to) {
+        isMoveValid = true;
+        this.game.move(desiredMove);
+      }
+    });
+
+    if (!isMoveValid) {
+      return "snapback";
+    }
+
+    console.log("OnDrop:", source, target, piece, newPos, oldPos, orientation);
+
+    if (this.gameMode == "hvc") {
+      this.timeoutID = setTimeout(this.computerMove.bind(this), 100);
+    }
+  }
+
+  computerMove() {
+    if (this.checkGameOver().over) {
+      console.log(this.checkGameOver());
+      return;
+    }
+
+    let moves = this.game.moves({ verbose: true });
+
+    let move = this.getRandomMove(moves);
+    this.game.move(move);
+    console.log(move);
+
+    let boardMove = move.from + "-" + move.to;
+    console.log(boardMove);
+    this.board.move(boardMove);
+
+    if (this.gameMode == "cvc") {
+      this.timeoutID = setTimeout(this.computerMove.bind(this), 100);
+    }
+  }
+
+  getRandomMove(moves) {
+    return moves[Math.floor(Math.random() * moves.length)];
+  }
+
+  reset() {
+    this.game.reset();
+    this.board.start();
+    clearTimeout(this.timeoutID);
+  }
+
+  run() {
+    if (this.gameMode == "cvc") {
+      this.timeoutID = setTimeout(this.computerMove.bind(this), 500);
+    }
+
+    if (this.gameMode == "hvc" && this.white == "c") {
+      this.timeoutID = setTimeout(this.computerMove.bind(this), 500);
+    }
+  }
+
+  checkGameOver() {
+    return {
+      over: this.game.game_over(),
+      checkmate: this.game.in_checkmate(),
+      draw: this.game.in_draw(),
+      stalemate: this.game.in_stalemate(),
+      insuff: this.game.insufficient_material(),
+      rep: this.game.in_threefold_repetition(),
+    };
+  }
+}
+
+const initializeGame = () => {
+  return new ChessGame(
+    $("#whitePlayer :selected").val(),
+    $("#blackPlayer :selected").val()
+  );
 };
+
+var chessGame = initializeGame();
+
+$("#whitePlayer").on("change", () => {
+  chessGame = initializeGame();
+});
+
+$("#blackPlayer").on("change", () => {
+  chessGame = initializeGame();
+});
+
+$("#resetButton").on("click", () => {
+  chessGame.reset();
+});
+$("#startButton").on("click", () => {
+  chessGame.run();
+});
+
+// class ComputerPlayer {
+//   constructor(algorithm, color) {
+//     this.algorithm = algorithm;
+//     this.color = color;
+//   }
+
+//   getMove(moves) {
+//     if (this.algorithm == "random") {
+//       return getRandomMove(moves);
+//     }
+//   }
+// }
