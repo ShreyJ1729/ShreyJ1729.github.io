5bc4d3779e507c873ba68604c5ff863dc1a5be64
Shrey Joshi <shreyjoshi2004@gmail.com>
2022-05-02 19:06:12 -0500

    update

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const updateAge = () => {
   const bday = new Date("Tue Aug 3 2004").getTime();
   const ageMs = Date.now() - bday;
   const age = ageMs / 1000 / 60 / 60 / 24 / 365.25;
-  document.getElementById("age").innerHTML = "Current Age: <strong>" + age.toPrecision(10) + "</strong>";
+  document.getElementById("age").innerHTML =
+    "Current Age: <strong>" + age.toPrecision(10) + "</strong>";
   requestAnimationFrame(updateAge);
 };
 
 const OpenLinksInNewTab = () => {
   const links = document.getElementsByTagName("a");
-  
+
   for (let i = 0; i < links.length; i++) {
     last = links[i].href.split("/").pop();
 
@@ -20,3 +21,5 @@ const OpenLinksInNewTab = () => {
 
 OpenLinksInNewTab();
 requestAnimationFrame(updateAge);
+
+document.getElementById("website-last-updated").innerHTML = "Website last updated: <b>" + document.lastModified + "</b>";
